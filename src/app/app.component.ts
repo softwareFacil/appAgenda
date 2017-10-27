71718b5f8e3a771fc6b4058ef06420531830a770
Patricio Torres <ptorresmanque@gmail.com>
2017-10-27 18:34:15 -0300

    funcionamiento menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TiposPage } from "../pages/tipos/tipos";
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -9,14 +10,21 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
+  home = HomePage;
+  tipos = TiposPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl:MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  abrirPage(pagina:any){
+    this.rootPage = pagina;
+    this.menuCtrl.toggle();
   }
 }
 
