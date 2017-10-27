6caeff143706e894a0f837bd7f6d145df258cb76
Patricio Torres <ptorresmanque@gmail.com>
2017-10-27 18:33:05 -0300

    implementacion del compartir

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { NavController, MenuController } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { ObtenerProvider } from "../../providers/obtener/obtener";
 import { StatusBar } from '@ionic-native/status-bar';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { Toast } from '@ionic-native/toast';
 
 @Component({
   selector: 'page-home',
@@ -12,7 +14,9 @@ export class HomePage {
 
   eventos:Observable<any[]>;
 
-  constructor(public navCtrl: NavController, private menuCtrl:MenuController, private _obs:ObtenerProvider, private statusBar: StatusBar) {
+  constructor(public navCtrl: NavController, private menuCtrl:MenuController,
+               private _obs:ObtenerProvider, private statusBar: StatusBar, 
+               private socialSharing: SocialSharing, private toast: Toast) {
     this.statusBar.backgroundColorByHexString('#4ad52e');
     this._obs.getEventos().subscribe();
   }
@@ -26,6 +30,22 @@ export class HomePage {
       this._obs.getEventos().subscribe();
       refresher.complete();
     }, 1000);
+  }
+
+  compartir(evento:any){
+    this.socialSharing.share(evento.name, evento.tipo, this.imagen(evento.image), "http://agenda.publibarrio.cl").then(()=>{
+      
+    }).catch(()=>{
+      this.toast.show(`No fue compartido!!`, '3000', 'center').subscribe(
+        toast => {
+          console.log(toast);
+        }
+      );
+    });
+  }
+
+  imagen(imagen:string){
+    return "http://agenda.publibarrio.cl:3789/api/get-img/" + imagen;
   }
 
 }
